8a7157ee692479878a4edd7b4ac88428410eade5
René Perschon <rperschon85@gmail.com>
2018-06-30 23:53:48 +0200

    RaftServer now inits from persisted file

--- src/raft_server.rs
+++ src/raft_server.rs
@@ -2,8 +2,12 @@ use std::sync::RwLock;
 use messages::{AppendEntriesRequest, AppendEntriesResponse};
 use std::cmp;
 use std::fs::File;
+use std::path::Path;
 use std::io::Write;
+use std::io::Read;
 use serde_json;
+
+const PERSISTENT_STORAGE_FILENAME: &'static str = "raft_persistent_state.json";
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum ServerStatus {
@@ -18,13 +22,22 @@ pub struct RaftServer {
 
 impl RaftServer {
     pub fn new() -> Self {
-        RaftServer {
-            status: ServerStatus::Follower,
-            persistent_state: RwLock::new(PersistentState {
+        let persistent_state = if Path::new(PERSISTENT_STORAGE_FILENAME).exists() {
+            let mut file = File::open(PERSISTENT_STORAGE_FILENAME).unwrap();
+            let mut bytes = vec![];
+            file.read_to_end(&mut bytes).unwrap();
+            serde_json::from_slice::<PersistentState>(&bytes).unwrap()
+        } else {
+            PersistentState {
                 current_term: 0,
                 voted_for: None,
                 log: vec![]
-            })
+            }
+        };
+
+        RaftServer {
+            status: ServerStatus::Follower,
+            persistent_state: RwLock::new(persistent_state)
         }
     }
 
@@ -36,7 +49,7 @@ impl RaftServer {
             persistent_state.current_term = cmp::max(persistent_state.current_term, message.term);
             term_response = persistent_state.current_term;
 
-            let mut file = File::create("raft_persistent_state.json").unwrap();
+            let mut file = File::create(PERSISTENT_STORAGE_FILENAME).unwrap();
             let json = serde_json::to_string(&*persistent_state).unwrap();
             file.write(json.as_bytes()).unwrap();
         }
